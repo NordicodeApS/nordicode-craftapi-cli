ad695a5ec05e1e594a44f3f5ac1f1687438e517b
Jess Stopa <jess@stopa.dk>
2022-10-18 13:57:21 +0200

    Support for https
    Also some linting

--- src/craftapi-cli.js
+++ src/craftapi-cli.js
@@ -1,85 +1,77 @@
 #!/usr/bin/env node
 
-'use strict';
+"use strict";
 
-const OpenApiMocker = require('open-api-mocker');
+const OpenApiMocker = require("open-api-mocker");
+const URLSchemaLoader = require("./url-loader");
 
-const URLSchemaLoader = require('./URLSchemaLoader');
+const { argv } = require("yargs")
+  .option("port", {
+    alias: "p",
+    description: "The port to bind on",
+    type: "number",
+    default: 5001,
+  })
+  .option("schema", {
+    alias: "s",
+    description: "The path or URL of the schema to mock",
+    type: "string",
+  })
+  .option("watch", {
+    type: "boolean",
+    alias: "w",
+    description: "Indicates if schema should be watched for changes or not",
+    default: false,
+  })
+  .option("verbose", {
+    type: "boolean",
+    alias: "v",
+    conflicts: "quiet",
+  })
+  .option("quiet", {
+    type: "boolean",
+    alias: "q",
+    description: "Suppress request output, only show errors",
+    conflicts: "verbose",
+  })
+  .demandOption("schema")
+  .help();
 
-const { argv } = require('yargs')
-	.option('port', {
-		alias: 'p',
-		description: 'The port to bind on',
-		type: 'number',
-		default: 5001
-	})
-	.option('schema', {
-		alias: 's',
-		description: 'The path or URL of the schema to mock',
-		type: 'string'
-	})
-	.option('watch', {
-		type: 'boolean',
-		alias: 'w',
-		description: 'Indicates if schema should be watched for changes or not',
-		default: false
-	})
-	.option('verbose', {
-		type: 'boolean',
-		alias: 'v',
-		conflicts: 'quiet'
-	})
-	.option('quiet', {
-		type: 'boolean',
-		alias: 'q',
-		description: 'Suppress request output, only show errors',
-		conflicts: 'verbose'
-	})
-	.demandOption('schema')
-	.help();
-
-const logger = require('lllog')();
+const logger = require("lllog")();
 
 const RUNTIME_ERROR = 90;
 
-if(argv.verbose)
-	logger.setMinLevel('debug');
-else if(argv.quiet)
-	logger.setMinLevel('error');
+if (argv.verbose) logger.setMinLevel("debug");
+else if (argv.quiet) logger.setMinLevel("error");
 
 (async () => {
+  let sigintReceived = false;
 
-	let sigintReceived = false;
-
-	try {
-		const openApiMocker = new OpenApiMocker({
-			port: argv.port,
-			schema: argv.schema,
-			watch: !!argv.watch,
+  try {
+    const openApiMocker = new OpenApiMocker({
+      port: argv.port,
+      schema: argv.schema,
+      watch: !!argv.watch,
       schemaLoader: new URLSchemaLoader(),
-		});
+    });
 
-		await openApiMocker.validate();
+    await openApiMocker.validate();
 
-		process.on('SIGINT', async () => {
+    process.on("SIGINT", async () => {
+      if (sigintReceived) process.exit(0);
 
-			if(sigintReceived)
-				process.exit(0);
+      sigintReceived = true;
 
-			sigintReceived = true;
+      logger.info("SIGINT received. Shutting down...");
+      logger.info("Press ^C again to force stop.");
 
-			logger.info('SIGINT received. Shutting down...');
-			logger.info('Press ^C again to force stop.');
+      await openApiMocker.shutdown();
+      process.exit(0);
+    });
 
-			await openApiMocker.shutdown();
-			process.exit(0);
-		});
-
-		await openApiMocker.mock();
-
-	} catch(e) {
-		logger.fatal(`Error while mocking schema: ${e.message}`);
-		process.exit(e.cliError || RUNTIME_ERROR);
-	}
-
+    await openApiMocker.mock();
+  } catch (e) {
+    logger.fatal(`Error while mocking schema: ${e.message}`);
+    process.exit(e.cliError || RUNTIME_ERROR);
+  }
 })();
---
+++ src/url-loader.js
@@ -0,0 +1,58 @@
+const EventEmitter = require("events");
+const http = require("http");
+const https = require("https");
+const YAML = require("js-yaml");
+const logger = require("lllog")();
+
+class URLSchemaLoader extends EventEmitter {
+  // SchemaOption is the value of the `schema` option or the value passed to the `setSchema` method. It's value depends on what your loader needs.
+  async load(schemaURL) {
+    if (!schemaURL.match(/^http/)) {
+      throw new Error(`Invalid schema URL: ${schemaURL}`);
+    }
+
+    let response = await this.makeSynchronousRequest(schemaURL);
+
+    return YAML.load(response);
+  }
+
+  watch() {
+    logger.info("Schema will update every 10 seconds...");
+
+    setInterval(async () => this.emit("schema-changed"), 10000);
+  }
+
+  makeSynchronousRequest(url) {
+    return new Promise((resolve, reject) => {
+      if (url.match(/^https/)) {
+        https.get(url, (response) => {
+          this.responseHandler(response, resolve, reject);
+        });
+      } else {
+        http.get(url, (response) => {
+          this.responseHandler(response, resolve, reject);
+        });
+      }
+    });
+  }
+
+  responseHandler(response, resolve, reject) {
+    let chunks_of_data = [];
+
+    response.on("data", (fragments) => {
+      chunks_of_data.push(fragments);
+    });
+
+    response.on("end", () => {
+      let response_body = Buffer.concat(chunks_of_data);
+
+      resolve(response_body.toString());
+    });
+
+    response.on("error", (error) => {
+      reject(error);
+    });
+  }
+}
+
+module.exports = URLSchemaLoader;
